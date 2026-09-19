554f10c6396fd20e9b45632aabcb4f8184c4e3bd
tamannaaira1078 <tamanna.islam05@northsouth.edu>
2026-09-19 13:16:54 +0600

    Added some feature on joke fetcher project

--- joke_fetcher.py
+++ joke_fetcher.py
@@ -1,9 +1,34 @@
 import requests
-url = "https://official-joke-api.appspot.com/random_joke"
-response = requests.get(url)
-if response.status_code==200:
-    data = response.json()
-    print(data["setup"])
-    print(data["punchline"])
-else:
-    print("Something went wrong!")    
+
+def get_joke():
+
+    url = "https://official-joke-api.appspot.com/random_joke"
+    response = requests.get(url)
+    if response.status_code==200:
+        data = response.json()
+        return data["setup"], data["punchline"]
+        
+            
+    else:
+        #return None for both setup and punchline if it fails
+        return None, None
+        
+
+
+while True:
+    user_choice = input("Do you want to continue?(yes/no): ").strip().lower() 
+    if user_choice == "yes":
+        setup, punchline = get_joke()
+        if setup is None:
+            print("Failed to fetch a joke")
+        else:
+            print(setup)
+            print(punchline)    
+        
+    elif user_choice == "no":
+        print("Goodbye")
+        break
+        
+    else:
+        print("Invalid Input.Please type yes or no.") 
+         
